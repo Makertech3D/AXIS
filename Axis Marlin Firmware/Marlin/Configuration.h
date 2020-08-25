d03ef13e0f208da9c092069e191d32a6ac02bd29
Makertech 3D <adeel@makertech3d.com>
2020-08-25 13:42:19 +0100

    Max temp limits update

--- Axis Marlin Firmware/Marlin/Configuration.h
+++ Axis Marlin Firmware/Marlin/Configuration.h
@@ -450,7 +450,7 @@
 // Above this temperature the heater will be switched off.
 // This can protect components from overheating, but NOT from shorts and failures.
 // (Use MINTEMP for thermistor short/failure protection.)
-#define HEATER_0_MAXTEMP 275
+#define HEATER_0_MAXTEMP 230
 #define HEATER_1_MAXTEMP 275
 #define HEATER_2_MAXTEMP 275
 #define HEATER_3_MAXTEMP 275
@@ -458,7 +458,7 @@
 #define HEATER_5_MAXTEMP 275
 #define HEATER_6_MAXTEMP 275
 #define HEATER_7_MAXTEMP 275
-#define BED_MAXTEMP      150
+#define BED_MAXTEMP      55
 
 #define HEATER_BED_PIN            RAMPS_D8_PIN
 
