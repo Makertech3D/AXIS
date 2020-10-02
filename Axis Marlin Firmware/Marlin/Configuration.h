077072e6155fbea71f2e22709196554fd656b8d9
Makertech 3D <adeel@makertech3d.com>
2020-10-02 13:16:47 +0100

    Extruder Steps Update
    Steps for extruder lowered for better quality printing.

--- Axis Marlin Firmware/Marlin/Configuration.h
+++ Axis Marlin Firmware/Marlin/Configuration.h
@@ -737,7 +737,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 100, 100, 400, 98 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 100, 100, 400, 85 }
 
 /**
  * Default Max Feed Rate (mm/s)
